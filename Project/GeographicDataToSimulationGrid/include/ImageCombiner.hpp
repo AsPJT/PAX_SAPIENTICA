7693e6d5c9af8a157e4243d209dad317c85a0c3f
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2023-04-15 15:01:10 +0900

    update: ImageCombiner

--- Project/GeographicDataToSimulationGrid/include/ImageCombiner.hpp
+++ Project/GeographicDataToSimulationGrid/include/ImageCombiner.hpp
@@ -16,52 +16,77 @@
 
 ##########################################################################################*/
 
+#include <filesystem>
 #include <iostream>
-#include <memory>
 
 #include <Helper.hpp>
 
 namespace paxs{
     constexpr int img_size = 256;
-    const std::string simulation_folder_path = "../../../Data/Simulation/";
 
     class ImageCombiner{
     public:
         ImageCombiner(){
 			paxs::Settings settings = paxs::Helper::getSettings();
-            img_num = std::stoi(settings["imgnum"]);
+
+            start_x = std::stoi(settings["start_x"]);
+            start_y = std::stoi(settings["start_y"]);
+            x_size = std::stoi(settings["x_size"]);
+            y_size = std::stoi(settings["y_size"]);
 
             std::string name = settings["name"];
             extension = settings["extension"];
 
-            path_prefix = simulation_folder_path + name + '/' + name + '-' + settings["z"] + '-';
-            output_path = simulation_folder_path + name + '.' + extension;
+            path_prefix = settings["input_path"] + name + '_' + settings["z"] + '_';
+            output_path = settings["output_path"] + name + '_' + settings["z"] + '_' + settings["start_x"] + '_' + settings["start_y"] + '_' + settings["x_size"] + '_' + settings["y_size"] + '.' + extension;
+
+            white_img = cv::Mat::zeros(img_size, img_size, CV_8UC3);
+            for (int j = 0; j < img_size; j++) {
+                for (int i = 0; i < img_size; i++) {
+                    white_img.at<cv::Vec3b>(j, i)[0] = 255; //青
+                    white_img.at<cv::Vec3b>(j, i)[1] = 255; //緑
+                    white_img.at<cv::Vec3b>(j, i)[2] = 255; //赤
+                }
+            }
         }
         void combine(){
-            cv::Mat combined_img = vCombine(0);
-            for(int x=1;x<img_num;x++){
-                cv::Mat tmp_img = vCombine(x);
+            cv::Mat combined_img = vCombine(start_x);
+            for(int x=1;x<x_size;x++){
+                cv::Mat tmp_img = vCombine(start_x + x);
                 cv::hconcat(combined_img, tmp_img, combined_img);
             }
 			cv::imwrite(output_path, combined_img);
         }
     private:
+        int start_x;
+        int start_y;
+        int x_size;
+        int y_size;
         std::string path_prefix;
         std::string output_path;
-        int img_num;
         std::string extension;
+        cv::Mat white_img;
 
         cv::Mat vCombine(const int x){
-            cv::Mat combined_img = cv::imread(getFilePath(x, 0));
-            for(int y=1;y<img_num;y++){
-			    cv::Mat img = cv::imread(getFilePath(x, y));
-                cv::vconcat(combined_img, img, combined_img);
+            cv::Mat combined_img = white_img;
+            std::string path = getFilePath(x, start_y);
+            if(std::filesystem::exists(path)){
+		        combined_img = cv::imread(path);
+            }
+            for(int y=0;y<y_size;y++){
+                std::string path = getFilePath(x, start_y + y);
+                if(std::filesystem::exists(path)){
+			        cv::Mat img = cv::imread(path);
+                    cv::vconcat(combined_img, img, combined_img);
+                    continue;
+                }
+                cv::vconcat(combined_img, white_img, combined_img);
             }
             return combined_img;
         }
 
         std::string getFilePath(int x, int y){
-            return path_prefix + std::to_string(x) + '-' + std::to_string(y) + '.' + extension;
+            return path_prefix + std::to_string(x) + '_' + std::to_string(y) + '.' + extension;
         }
     };
 }
