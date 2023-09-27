3c3f102d57ce2a9b44fe3a412435b554e8567e06
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2023-09-27 12:15:49 +0900

    upd: 画面のリサイズ

--- Library/PAX_GRAPHICA/Window.hpp
+++ Library/PAX_GRAPHICA/Window.hpp
@@ -68,6 +68,14 @@ namespace paxg {
                 if (event.type == sf::Event::Closed)
                     return false;
             }
+
+            if (event.type == sf::Event::Resized)
+            {
+                // update the view to the new size of the window
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+            }
+
             return true;
 #elif defined(PAXS_USING_DXLIB)
             return (DxLib::ScreenFlip() != -1 &&
