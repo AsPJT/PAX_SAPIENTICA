42e36b21144f0f0a7b9ce068ac8e406d2c6eb541
wanotaitei <wanotaitei@gmail.com>
2023-10-24 19:10:33 +0900

    android: new file gen

--- Project/Android/app/src/main/cpp/native-lib.cpp
+++ Project/Android/app/src/main/cpp/native-lib.cpp
@@ -9,13 +9,17 @@
 
 ##########################################################################################*/
 
-#define PAXS_PATH "./"
+#define PAXS_PATH ""
 #define PAXS_USING_DXLIB
 #include <DxLib.h>
 
 #include <PAX_GRAPHICA/Window.hpp>
 #include <PAX_MAHOROBA/Main.hpp>
 #include <PAX_SAPIENTICA/Type/Vector2.hpp>
+
+#include <filesystem>
+#include <iostream>
+#include <fstream>
 
 int android_main() {
     DxLib::SetUseCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
@@ -25,17 +29,23 @@ int android_main() {
     DxLib::GetAndroidDisplayResolution(&w, &h);
     DxLib::SetGraphMode(w, h, 32);
 
+    char file_path[256]{};
+    std::string str;
+    DxLib::GetExternalDataPath( file_path, sizeof( file_path ) ) ;
+    str = std::string(file_path) + std::string("/PAX_SAPIENTICA");
+
+    std::filesystem::create_directories(str);
+    std::ofstream ofs(str + "/Test.txt");
+    if (!ofs) printfDx("False");
+    else{
+        printfDx("%s",str.c_str());
+        ofs << "Test" << std::endl;
+    }
+
     // 背景色を指定
     const paxg::Color color = paxg::Color(140, 180, 250); // 青
     paxg::Window::setBackgroundColor(color);
-
-    int m = DxLib::LoadGraph("./Data/Map/XYZTile/Slope/Image/SlopePale20230920/2010/0/slope_pale_0_0_0.png");
-
     DxLib::SetDrawScreen(DX_SCREEN_BACK);
-    // while (paxg::Window::update()) {
-        DrawBox(0, 0, 300, 100, GetColor(230, 230, 240), TRUE);    // 四角形を描画する
-        DrawGraph(0,0,m,TRUE);
-    // }
     paxs::startMain(PAXS_PATH);
 
     return DxLib_End();
