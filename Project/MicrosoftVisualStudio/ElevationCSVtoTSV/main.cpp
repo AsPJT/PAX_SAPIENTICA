96b72477f00e28980ab26c196c2b7b3495567a54
wanotaitei <wanotaitei@gmail.com>
2023-10-28 23:32:12 +0900

    add: tsv to binary

--- Project/MicrosoftVisualStudio/ElevationCSVtoTSV/main.cpp
+++ Project/MicrosoftVisualStudio/ElevationCSVtoTSV/main.cpp
@@ -10,7 +10,7 @@
 ##########################################################################################*/
 #define _CRT_SECURE_NO_WARNINGS
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include <stb/stb_image_write.h>
 
 #include <array>
 #include <cstddef>
---
+++ Project/MicrosoftVisualStudio/XYZTilesTSVtoBinary/main.cpp
@@ -0,0 +1,33 @@
+﻿/*##########################################################################################
+
+    PAX SAPIENTICA Library 💀🌿🌏
+
+    [Planning]		2023 As Project
+    [Production]	2023 As Project
+    [Contact Us]	wanotaitei@gmail.com			https://github.com/AsPJT/PAX_SAPIENTICA
+    [License]		Distributed under the CC0 1.0.	https://creativecommons.org/publicdomain/zero/1.0/
+
+##########################################################################################*/
+
+
+#include <PAX_SAPIENTICA/GeographicInformation/Slope.hpp>
+
+#include <iostream>
+
+int  main() {
+
+    paxs::SlopeDegU0To250AllOutput sda(
+        "Data/Map/XYZTile/Slope/Data/SlopeData20230514/2010/8/slope_8_0_63.tsv", "./../../../",
+        "slope_test4.bin", "./", '\t'
+    );
+    sda.calc();
+
+    //paxg::InputFile ifs("Data/Map/XYZTile/Slope/Data/SlopeData20230514/2010/8/slope_8_0_53.tsv", "./../../../");
+    //paxg::InputFile ifs("Data/Map/XYZTile/Slope/Data/SlopeData20230514/2010/8/slope_8_179_109.tsv", "./../../../");
+    //paxg::InputFile ifs("Data/Map/XYZTile/Slope/Data/SlopeData20230514/2010/8/slope_8_0_84.tsv", "./../../../");
+
+
+    std::cout << "\n" << sda.sd.tmp_count;
+    std::cout << "\n" << sda.sd.tmp_count2;
+
+}
