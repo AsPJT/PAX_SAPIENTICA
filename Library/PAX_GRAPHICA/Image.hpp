3fc85d344fe156b12656b2fa18d5787c00b9d838
wanotaitei <wanotaitei@gmail.com>
2024-01-01 01:17:59 +0900

    upd: 2023-2024

--- Library/PAX_GRAPHICA/Image.hpp
+++ Library/PAX_GRAPHICA/Image.hpp
@@ -2,8 +2,8 @@
 
     PAX SAPIENTICA Library 💀🌿🌏
 
-    [Planning]		2023 As Project
-    [Production]	2023 As Project
+    [Planning]		2023-2024 As Project
+    [Production]	2023-2024 As Project
     [Contact Us]	wanotaitei@gmail.com			https://github.com/AsPJT/PAX_SAPIENTICA
     [License]		Distributed under the CC0 1.0.	https://creativecommons.org/publicdomain/zero/1.0/
 
