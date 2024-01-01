4d3e9f0f77e32a75563bc73171e327c14e5298cb
wanotaitei <wanotaitei@gmail.com>
2024-01-01 21:22:57 +0900

    add: Windows SFML

---
+++ Projects/MapViewer/Windows/SFML_2.6.1/Main.cpp
@@ -0,0 +1,24 @@
+﻿/*##########################################################################################
+
+    PAX SAPIENTICA Library 💀🌿🌏
+
+    [Planning]		2023-2024 As Project
+    [Production]	2023-2024 As Project
+    [Contact Us]	wanotaitei@gmail.com			https://github.com/AsPJT/PAX_SAPIENTICA
+    [License]		Distributed under the CC0 1.0.	https://creativecommons.org/publicdomain/zero/1.0/
+
+##########################################################################################*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define PAXS_PATH "./../../../../"
+#define PAXS_USING_SFML
+#include <SFML/Graphics.hpp>
+#include <omp.h>
+#include <PAX_MAHOROBA/Main.hpp>
+
+// その他の処理 (Windows)
+int main() {
+
+    paxs::startMain(PAXS_PATH);
+    return 0;
+}
