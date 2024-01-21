eca09ae1351243d1067731d120eab6ba14f48ff0
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2024-01-21 22:58:50 +0900

    ref: rename

---
+++ Projects/IntegrationTest/Source/SimulatorTest.cpp
@@ -0,0 +1,22 @@
+﻿/*##########################################################################################
+
+	PAX SAPIENTICA Library 💀🌿🌏
+
+	[Planning]		2023-2024 As Project
+	[Production]	2023-2024 As Project
+	[Contact Us]	wanotaitei@gmail.com			https://github.com/AsPJT/PAX_SAPIENTICA
+	[License]		Distributed under the CC0 1.0.	https://creativecommons.org/publicdomain/zero/1.0/
+
+##########################################################################################*/
+
+#include <memory>
+
+#include <PAX_SAPIENTICA/Simulation/Simulator.hpp>
+
+int main() {
+	const std::string setting_file_path = "../../../Data/Simulations/MapList.tsv";
+    std::unique_ptr<paxs::Simulator<int>> simulator = std::make_unique<paxs::Simulator<int>>(setting_file_path, 10);
+    simulator->init();
+	simulator->run(50);
+	std::vector<paxs::Agent<int>> agents = simulator->getAgents();
+}
