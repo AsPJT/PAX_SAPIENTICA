c38a54f4588cf3cc0830d756b8b8d38664528812
guinpen98 <guinpen9988@gmail.com>
2025-10-30 10:29:20 +0900

    fix: Key.hpp

--- Library/PAX_GRAPHICA/Key.hpp
+++ Library/PAX_GRAPHICA/Key.hpp
@@ -19,6 +19,14 @@
 #include <new>
 #include <vector>
 
+#if defined(PAXS_USING_SIV3D)
+#include <Siv3D.hpp>
+#elif defined(PAXS_USING_DXLIB)
+#include <DxLib.h>
+#elif defined(PAXS_USING_SFML)
+#include <SFML/Graphics.hpp>
+#endif
+
 #include <PAX_GRAPHICA/Mouse.hpp>
 #include <PAX_GRAPHICA/Window.hpp>
 
@@ -75,8 +83,7 @@ namespace paxs {
 #endif
     };
 
-    // X-Macro pattern: Define all keys once
-    #define PAXG_KEY_LIST(X) \
+#define PAXG_KEY_LIST(X) \
         X(0) X(1) X(2) X(3) X(4) X(5) X(6) X(7) X(8) X(9) \
         X(A) X(B) X(C) X(D) X(E) X(F) X(G) X(H) X(I) X(J) \
         X(K) X(L) X(M) X(N) X(O) X(P) X(Q) X(R) X(S) X(T) \
@@ -86,23 +93,48 @@ namespace paxs {
 #if defined(PAXS_USING_SIV3D)
     using InputKey = s3d::Input;
 
-    // Siv3D: Direct mapping to s3d::Key*
-    #define PAXG_DEFINE_KEY(name) constexpr InputKey PAXG_KEY_##name = s3d::Key##name;
-    PAXG_KEY_LIST(PAXG_DEFINE_KEY)
-    #undef PAXG_DEFINE_KEY
+#define PAXG_DEFINE_KEY_COMMON(name) inline constexpr InputKey PAXG_KEY_##name = s3d::Key##name;
+    PAXG_DEFINE_KEY_COMMON(0) PAXG_DEFINE_KEY_COMMON(1) PAXG_DEFINE_KEY_COMMON(2) PAXG_DEFINE_KEY_COMMON(3)
+    PAXG_DEFINE_KEY_COMMON(4) PAXG_DEFINE_KEY_COMMON(5) PAXG_DEFINE_KEY_COMMON(6) PAXG_DEFINE_KEY_COMMON(7)
+    PAXG_DEFINE_KEY_COMMON(8) PAXG_DEFINE_KEY_COMMON(9)
+    PAXG_DEFINE_KEY_COMMON(A) PAXG_DEFINE_KEY_COMMON(B) PAXG_DEFINE_KEY_COMMON(C) PAXG_DEFINE_KEY_COMMON(D)
+    PAXG_DEFINE_KEY_COMMON(E) PAXG_DEFINE_KEY_COMMON(F) PAXG_DEFINE_KEY_COMMON(G) PAXG_DEFINE_KEY_COMMON(H)
+    PAXG_DEFINE_KEY_COMMON(I) PAXG_DEFINE_KEY_COMMON(J) PAXG_DEFINE_KEY_COMMON(K) PAXG_DEFINE_KEY_COMMON(L)
+    PAXG_DEFINE_KEY_COMMON(M) PAXG_DEFINE_KEY_COMMON(N) PAXG_DEFINE_KEY_COMMON(O) PAXG_DEFINE_KEY_COMMON(P)
+    PAXG_DEFINE_KEY_COMMON(Q) PAXG_DEFINE_KEY_COMMON(R) PAXG_DEFINE_KEY_COMMON(S) PAXG_DEFINE_KEY_COMMON(T)
+    PAXG_DEFINE_KEY_COMMON(U) PAXG_DEFINE_KEY_COMMON(V) PAXG_DEFINE_KEY_COMMON(W) PAXG_DEFINE_KEY_COMMON(X)
+    PAXG_DEFINE_KEY_COMMON(Y) PAXG_DEFINE_KEY_COMMON(Z)
+    constexpr InputKey PAXG_KEY_LEFT = s3d::KeyLeft;
+    constexpr InputKey PAXG_KEY_RIGHT = s3d::KeyRight;
+    constexpr InputKey PAXG_KEY_DOWN = s3d::KeyDown;
+    constexpr InputKey PAXG_KEY_UP = s3d::KeyUp;
+#undef PAXG_DEFINE_KEY_COMMON
 
 #elif defined(PAXS_USING_DXLIB)
     using InputKey = InputStruct;
 
-    // DxLib: Wrap KEY_INPUT_* constants
-    #define PAXG_DEFINE_KEY(name) inline const InputKey PAXG_KEY_##name = InputKey{KEY_INPUT_##name};
-    PAXG_KEY_LIST(PAXG_DEFINE_KEY)
-    #undef PAXG_DEFINE_KEY
+#define PAXG_DXLIB_KEY(name) \
+    inline const InputKey PAXG_KEY_##name = InputKey{ KEY_INPUT_##name };
+
+    PAXG_DXLIB_KEY(0)  PAXG_DXLIB_KEY(1)  PAXG_DXLIB_KEY(2)  PAXG_DXLIB_KEY(3)
+    PAXG_DXLIB_KEY(4)  PAXG_DXLIB_KEY(5)  PAXG_DXLIB_KEY(6)  PAXG_DXLIB_KEY(7)
+    PAXG_DXLIB_KEY(8)  PAXG_DXLIB_KEY(9)
+    PAXG_DXLIB_KEY(A)  PAXG_DXLIB_KEY(B)  PAXG_DXLIB_KEY(C)  PAXG_DXLIB_KEY(D)
+    PAXG_DXLIB_KEY(E)  PAXG_DXLIB_KEY(F)  PAXG_DXLIB_KEY(G)  PAXG_DXLIB_KEY(H)
+    PAXG_DXLIB_KEY(I)  PAXG_DXLIB_KEY(J)  PAXG_DXLIB_KEY(K)  PAXG_DXLIB_KEY(L)
+    PAXG_DXLIB_KEY(M)  PAXG_DXLIB_KEY(N)  PAXG_DXLIB_KEY(O)  PAXG_DXLIB_KEY(P)
+    PAXG_DXLIB_KEY(Q)  PAXG_DXLIB_KEY(R)  PAXG_DXLIB_KEY(S)  PAXG_DXLIB_KEY(T)
+    PAXG_DXLIB_KEY(U)  PAXG_DXLIB_KEY(V)  PAXG_DXLIB_KEY(W)  PAXG_DXLIB_KEY(X)
+    PAXG_DXLIB_KEY(Y)  PAXG_DXLIB_KEY(Z)
+    PAXG_DXLIB_KEY(LEFT)
+    PAXG_DXLIB_KEY(RIGHT)
+    PAXG_DXLIB_KEY(DOWN)
+    PAXG_DXLIB_KEY(UP)
+#undef PAXG_DXLIB_KEY
 
 #elif defined(PAXS_USING_SFML)
     using InputKey = InputStruct;
 
-    // SFML: Map to sf::Keyboard::Key::* (with special handling for numbers)
     inline const InputKey PAXG_KEY_0 = InputKey{sf::Keyboard::Key::Num0};
     inline const InputKey PAXG_KEY_1 = InputKey{sf::Keyboard::Key::Num1};
     inline const InputKey PAXG_KEY_2 = InputKey{sf::Keyboard::Key::Num2};
@@ -113,15 +145,15 @@ namespace paxs {
     inline const InputKey PAXG_KEY_7 = InputKey{sf::Keyboard::Key::Num7};
     inline const InputKey PAXG_KEY_8 = InputKey{sf::Keyboard::Key::Num8};
     inline const InputKey PAXG_KEY_9 = InputKey{sf::Keyboard::Key::Num9};
-    #define PAXG_DEFINE_KEY(name) inline const InputKey PAXG_KEY_##name = InputKey{sf::Keyboard::Key::name};
-    PAXG_DEFINE_KEY(A) PAXG_DEFINE_KEY(B) PAXG_DEFINE_KEY(C) PAXG_DEFINE_KEY(D)
-    PAXG_DEFINE_KEY(E) PAXG_DEFINE_KEY(F) PAXG_DEFINE_KEY(G) PAXG_DEFINE_KEY(H)
-    PAXG_DEFINE_KEY(I) PAXG_DEFINE_KEY(J) PAXG_DEFINE_KEY(K) PAXG_DEFINE_KEY(L)
-    PAXG_DEFINE_KEY(M) PAXG_DEFINE_KEY(N) PAXG_DEFINE_KEY(O) PAXG_DEFINE_KEY(P)
-    PAXG_DEFINE_KEY(Q) PAXG_DEFINE_KEY(R) PAXG_DEFINE_KEY(S) PAXG_DEFINE_KEY(T)
-    PAXG_DEFINE_KEY(U) PAXG_DEFINE_KEY(V) PAXG_DEFINE_KEY(W) PAXG_DEFINE_KEY(X)
-    PAXG_DEFINE_KEY(Y) PAXG_DEFINE_KEY(Z)
-    #undef PAXG_DEFINE_KEY
+#define PAXG_SFML_ALPHA(name) inline const InputKey PAXG_KEY_##name = InputKey{ sf::Keyboard::Key::name };
+    PAXG_SFML_ALPHA(A) PAXG_SFML_ALPHA(B) PAXG_SFML_ALPHA(C) PAXG_SFML_ALPHA(D)
+    PAXG_SFML_ALPHA(E) PAXG_SFML_ALPHA(F) PAXG_SFML_ALPHA(G) PAXG_SFML_ALPHA(H)
+    PAXG_SFML_ALPHA(I) PAXG_SFML_ALPHA(J) PAXG_SFML_ALPHA(K) PAXG_SFML_ALPHA(L)
+    PAXG_SFML_ALPHA(M) PAXG_SFML_ALPHA(N) PAXG_SFML_ALPHA(O) PAXG_SFML_ALPHA(P)
+    PAXG_SFML_ALPHA(Q) PAXG_SFML_ALPHA(R) PAXG_SFML_ALPHA(S) PAXG_SFML_ALPHA(T)
+    PAXG_SFML_ALPHA(U) PAXG_SFML_ALPHA(V) PAXG_SFML_ALPHA(W) PAXG_SFML_ALPHA(X)
+    PAXG_SFML_ALPHA(Y) PAXG_SFML_ALPHA(Z)
+#undef PAXG_SFML_ALPHA
     inline const InputKey PAXG_KEY_LEFT = InputKey{sf::Keyboard::Key::Left};
     inline const InputKey PAXG_KEY_RIGHT = InputKey{sf::Keyboard::Key::Right};
     inline const InputKey PAXG_KEY_DOWN = InputKey{sf::Keyboard::Key::Down};
@@ -137,7 +169,7 @@ namespace paxs {
 
 #endif
 
-    #undef PAXG_KEY_LIST
+#undef PAXG_KEY_LIST
 
     class Key {
     public:
