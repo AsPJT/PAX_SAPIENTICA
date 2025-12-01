43adef382d4e67fe30eec1ac4d76c08b07ae0859
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2025-12-01 20:50:27 +0900

    fix: sfml build

--- Scripts/ProductionBuild.sh
+++ Scripts/ProductionBuild.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ROOT_PATH=$(dirname $(dirname "$0"))
+# Get absolute path to project root
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOT_PATH="$(cd "${SCRIPT_DIR}/.." && pwd)"
 cd "${ROOT_PATH}"
 BUILD_DIR_NAME="ProductionBuild"
 
 rm -rf "${BUILD_DIR_NAME}"
 mkdir "${BUILD_DIR_NAME}"
 
-cmake -S"Projects/MapViewer" -B"${BUILD_DIR_NAME}" -DCMAKE_BUILD_TYPE=Release -DPAXS_BUILD_TYPE=Production
+cmake -S"Projects/MapViewer" -B"${BUILD_DIR_NAME}" -DCMAKE_BUILD_TYPE=Release -DPAXS_BUILD_TYPE=Production -DSFML_STATIC_LIBRARIES=TRUE -DCMAKE_TOOLCHAIN_FILE="${ROOT_PATH}/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_INSTALLED_DIR="${ROOT_PATH}/vcpkg_installed"
 cmake --build "${BUILD_DIR_NAME}"
