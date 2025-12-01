cb5246946cf0948529656d6ba631ece77a686ba0
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2025-12-02 01:04:00 +0900

    upd: dmg

--- Scripts/ProductionBuild.sh
+++ Scripts/ProductionBuild.sh
@@ -11,3 +11,13 @@ mkdir "${BUILD_DIR_NAME}"
 
 cmake -S"Projects/MapViewer" -B"${BUILD_DIR_NAME}" -DCMAKE_BUILD_TYPE=Release -DPAXS_BUILD_TYPE=Production -DSFML_STATIC_LIBRARIES=TRUE -DCMAKE_TOOLCHAIN_FILE="${ROOT_PATH}/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_INSTALLED_DIR="${ROOT_PATH}/vcpkg_installed"
 cmake --build "${BUILD_DIR_NAME}"
+
+echo "Build completed."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo ".app bundle is located at: ${BUILD_DIR_NAME}/"
+    echo ""
+    echo "To create DMG after modifying the app bundle, run:"
+    echo "./Scripts/CreateDMG.sh"
+else
+    echo "Binary is located at: ${BUILD_DIR_NAME}/"
+fi
